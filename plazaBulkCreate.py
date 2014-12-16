6e94413b9ae2d521523f6b661787e175a96284eb
Aritza <aritzasm@gmail.com>
2014-12-16 03:13:34 +0100

    mejorando

--- plazaBulkCreate.py
+++ plazaBulkCreate.py
@@ -1,58 +1,81 @@
 #!/usr/bin/python
-# PlazaPodemos: Script para la creacion de plazas ;)
-# Datos necesarios: 'NombreCorto', 'nombre largo', 'una descripcion del sitio' 
-#
+# PlazaPodemos, Creacion de multiples sub-reddits a partir de lista desde archivo
+# soportevotaciones@podemos.info
+# Aritza, 16/10/2014
+# -*- coding: utf-8 -*-
 
 import praw
 import time
+import unicodedata
+import os
+
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
+# Datos para la etiqueta
+subReddit=''
+
+prefijo ='PlazaPodemos'
+prefijo2 = 'Plaza'
+
+prefijoLargo = u'Plaza Podemos de '
+descripcion = u'podemos!'
+
+nombreCorto = ''
+nombreLargo = ''
 
 # Contrasenas
-
-adminUser = ''
+adminUser = 'PlazaPodemos'
 adminPass = ''
 
 
-# Definiciones para los archivos
 
-fListaPlazas = 'fListaPlazas.txt'
-listaPlazas = []
+# Definiciones para el arichivo 
+fLista = 'municipiosParcial.txt'
+lista = []
 
 # Algunos contadores
-
+numOk = 0
 numError=0
-numPlazas=0
 
 # Cargamos los datos
-
-print( 'Podemos!' )
-print( 'Cargando datos...' )
-
-# Inicializo los objetos para la api
+os.system('clear')
+print('Podemos!')
 
 r = praw.Reddit(user_agent='FlairBotPodemos')
-r.login(adminUser, adminPass)
+r.login( adminUser, adminPass )
 
-with open(fListaPlazas) as f:
-	listaPlazas = f.read().splitlines()
-
-print( 'Creando plazas...' )
+with open(fLista) as f:
+	lista = f.read().splitlines()
 
 
-# Crea nuevas Plazas
+print('La lista de las Plazas....')
 
-for w in listaCG:
+for w in lista:
 	try:
-		# r.get_subreddit('podemos').set_flair(w,'Comision garantias','cg')
-	
-	except praw.errors.InvalidFlairTarget:
-		print 'Error: El usuario: ',w,' no existe'
-		numError=numError+1
+		
+		nombreCorto = prefijo + strip_accents ( w.decode('utf-8').replace(' ','') )
+		nombreLargo = prefijoLargo + strip_accents( w.decode('utf-8') )
+		
+		os.system('clear')
+		print 'Creando: ' + nombreCorto
+		print 'Nombre Largo: ' + nombreLargo
+		
+		print (' La creo? (s/n) ')
+		respuesta = raw_input()
+
+		if respuesta == "s" : 
+			print 'Creando...'
+			r.create_subreddit( nombreCorto, nombreLargo, language='es',subreddit_type='private', content_options='any', over_18=False, show_media=True )
+
+	except praw.errors.InvalidSubreddit:
+		print 'Error: en el subReddit: ',w,
+		numError = numError + 1
 	else:
-		numCG=numPlazas+1
+		numOk = numOk + 1
 
-# Resultados finales 
-
-print( 'Creaciones con exito' )
-print( 'Han ido bien', numPlazas )
-print( 'Erroneas: ', numError ) 
-
+# Presenta algunos resultados:
+print "Asignaciones con exito:", numOk
+print "Asignaciones erroneas:", numError				
