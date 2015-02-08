8445610e1699df87f522513a73c4a3cb18596dd4
Aritza <aritzasm@gmail.com>
2015-02-08 12:33:07 +0100

    beta update

---
+++ copyFlairs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+ # PlazaPodemos, copia asignaciones de etiquetas a plazas 
+ # Hay que editar los valores iniciales para que los use en la asignacion
+ # Aritza.
+
+import praw
+import time
+
+# Contrasenas
+
+adminUser = 'aritza'
+adminPass = ''
+
+# Valores iniciales 
+
+plazaOrigen = 'podemos'
+plazaDestino = ''
+
+
+# Definiciones para el archivo
+
+distintivo ='cc'
+
+# Algunos contadores y valores
+
+numError = 0
+numOk = 0
+
+# Cargamos los datos
+
+print('Podemos!')
+print 'recogiendo distintivos de ', plazaOrigen
+
+r = praw.Reddit(user_agent='FlairBotPodemos')
+r.login(adminUser, adminPass)
+
+listaDistintivos = r.get_subreddit(plazaOrigen).get_flair_list(limit=None)
+
+flairMap = []
+
+for i in listaDistintivos:
+	if i['flair_css_class'] == distintivo:
+		flairMap.append(i)
+		numOk = numOk + 1
+
+
+#r.get_subreddit(plazaDestino).set_flair_csv(flairMap)
+
+print "Asignaciones con exito:", numOk
+print "Asignaciones erroneas:", numError				
+
