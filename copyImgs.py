23f5be1c9b1c56f85eca102af69c3fcd6a765bb4
Aritza <aritzasm@gmail.com>
2015-02-09 12:04:54 +0100

    algunas correciones

--- copyImgs.py
+++ copyImgs.py
@@ -7,6 +7,7 @@
 import praw
 import argparse
 import urllib
+from urllib2 import urlopen, HTTPError
 import os
 
 # Contrasenas
@@ -17,6 +18,7 @@ adminPass = ''
 plazaOrigen = ''
 plazaDestino = ''
 imgDir ='images/'
+numOk=0
 
 # Un par de funciones para tratar la cache de imagenes
 def CargaImagenes():
@@ -35,7 +37,7 @@ def BorraCache():
 # Opciones
 parser = argparse.ArgumentParser(description='Copiamos las imagnes de una plaza a otra')
 parser.add_argument('-o','--origen', default='podemos', help='Plaza origen, de donde lee el CSS')
-parser.add_argument('-d','--destino', help='Plaza destieno, donde mete el CSS')
+parser.add_argument('-d','--destino', help='Plaza destino, donde sube las imagenes. Tambien lista separada por comas')
 parser.add_argument('-u','--user', help='especifica un usuario moderador')
 parser.add_argument('-p','--password', help='contrasena del moderador')
 args = parser.parse_args()
@@ -46,7 +48,7 @@ if args.destino:
 	#Inicializamos las varianles. 
 
 	plazaOrigen = args.origen
-	plazaDestino = args.destino
+	plazas = args.destino.split(',')
 	adminUser = args.user
 	adminPass = args.password
 
@@ -58,14 +60,25 @@ if args.destino:
 
 	print 'Bajando las imagenes de',plazaOrigen
 	CargaImagenes()
-	print 'Subiendo las imagenes para: ', plazaDestino
-	for img in images:
-		filepath = imgDir + img['name'] + '.png'
-		print 'Subiendo imagen: ',filepath
-		r.upload_image(plazaDestino,filepath,name=img['name'],header=False)
+	
+	for plazaDestino in plazas:
+		print 'Subiendo las imagenes para: ', plazaDestino
+		for img in images:
+			filepath = imgDir + img['name'] + '.png'
+			print 'Subiendo imagen: ',filepath
+			try:
+				r.upload_image(plazaDestino,filepath,name=img['name'],header=False)
+			except urllib2.URLError:
+				print 'Vaya, parece que reddit no responde, espero 1 minuto ...'
+				time.sleep(60)
+				pass
+
+			numOk = numOk + 1
+	
 	
 	BorraCache()
 	print 'Hecho!'
+	print numOk, 'Plazas actualizadas'
 
 else:
 	print ('Error: faltan argumentos')
