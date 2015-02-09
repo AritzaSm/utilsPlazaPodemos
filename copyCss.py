b1f0087b31c46fc90d871c309a2e58bf55bbd3b9
Aritza <aritzasm@gmail.com>
2015-02-09 12:02:51 +0100

    algunas correciones

--- copyCss.py
+++ copyCss.py
@@ -12,15 +12,16 @@ adminUser = ''
 adminPass = ''
 
 # Valores iniciales 
-plazaOrigen = 'podemos'
+plazaOrigen = ''
 plazaDestino = ''
+numOk = 0
 
 # Opciones
 parser = argparse.ArgumentParser(description='Copiamos un css de una plaza a otra')
 
 
 parser.add_argument('-o','--origen', default='podemos', help='Plaza origen, de donde lee el CSS')
-parser.add_argument('-d','--destino', help='Plaza destieno, donde mete el CSS')
+parser.add_argument('-d','--destino', help='Plaza destieno, donde sube el css. Tambien admite lista separada por comas')
 parser.add_argument('-u','--user', help='especifica un usuario moderador')
 parser.add_argument('-p','--password', help='contrasena del moderador')
 args = parser.parse_args()
@@ -31,20 +32,30 @@ if args.destino:
 	#Inicializamos las varianles. 
 
 	plazaOrigen = args.origen
-	plazaDestino = args.destino
+	plazas = args.destino.split(',')
 	adminUser = args.user
 	adminPass = args.password
+
 
 	# Cargamos los datos
 	r = praw.Reddit(user_agent='PlazaPodemosBot')
 	r.login( adminUser, adminPass )
 	r.config.decode_html_entities = True
-
-	print 'Estableciendo estilo en', plazaDestino
 	estilo = r.get_stylesheet(plazaOrigen)['stylesheet']
-	r.set_stylesheet(plazaDestino, estilo)
-	print'Hecho ; )'
+
+	for plazaDestino in plazas:
+		print 'Estableciendo estilo en', plazaDestino
+		try:
+			r.set_stylesheet(plazaDestino, estilo)
+		except requests.exceptions.ReadTimeout:
+			print 'Vaya, parece que reddit no responde, espero 1 minuto ...'
+			time.sleep(60)
+			pass
+
+		numOk = numOk + 1
 	
+	print'Hecho ; )'
+	print numOk,'Plazas actualizadas'	
 
 else:
 	print ('Error: faltan argumentos')
